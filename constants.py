3de28d928cde25e5e8e042dd0bfb8b51a0d77a26
Milov Dmirtiy <milovdd@mail.ru>
2023-06-22 02:31:50 +0300

    restore coeff val

--- constants.py
+++ constants.py
@@ -1,3 +1,3 @@
 redmine_activities_map = {"Бизнес-анализ": 8, "Разработка": 9, "Code review": 57, "Внутренние консультации": 58}
 redmine_url = "https://redmine.sbps.ru/"
-coefficient = 1.03
+coefficient = 1.00
